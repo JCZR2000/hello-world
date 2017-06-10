7fc3b2d45042e067d9eed5cdcae73b242886951e
Juan Carlos Zambrano <JCZR2000@users.noreply.github.com>
2017-06-09 21:34:40 -0400

    Actualización a la versión 0.5
    Añadido la participación del usuario y mejoras al archivo batch.

--- batch/helloworld.bat
+++ batch/helloworld.bat
@@ -1,4 +1,19 @@
 @echo off
-title Hola mundo 0.2
-echo Hola mundo! Version 0.2
-pause
+title Hola mundo! 0.5
+echo Hola mundo! Version 0.5
+echo.
+echo Presiona cualquier tecla para seguir...
+pause > nul
+cls
+echo Bienvenido, como te llamas?
+echo.
+set /p nombre= Escribe tu nombre:
+echo.
+echo Bien! Ahora presiona cualquier tecla para continuar...
+pause > nul
+cls
+echo Bienvenido %nombre%, ahora que sabemos tu nombre podemos
+echo continuar con el proceso de instalacion de Hola Mundo!.
+echo.
+echo Si quieres continuar, solo presiona cualquier tecla.
+pause > nul
