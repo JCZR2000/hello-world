e4eaa2545c5ec09a18a2aec886aef705c91173b0
Juan Carlos Zambrano <JCZR2000@users.noreply.github.com>
2017-06-09 21:58:20 -0400

    Actualización a la versión 0.6
    Añadidos menús y presentación del usuario.

--- batch/helloworld.bat
+++ batch/helloworld.bat
@@ -1,6 +1,6 @@
 @echo off
-title Hola mundo! 0.5
-echo Hola mundo! Version 0.5
+title Hola mundo! 0.6
+echo Hola mundo! Version 0.6
 echo.
 echo Presiona cualquier tecla para seguir...
 pause > nul
@@ -13,7 +13,18 @@ echo Bien! Ahora presiona cualquier tecla para continuar...
 pause > nul
 cls
 echo Bienvenido %nombre%, ahora que sabemos tu nombre podemos
-echo continuar con el proceso de instalacion de Hola Mundo!.
+echo continuar con el proceso de configuracion de Hola Mundo!.
 echo.
 echo Si quieres continuar, solo presiona cualquier tecla.
 pause > nul
+cls
+echo %nombre%, Bienvenido a Hola Mundo!
+echo.
+echo Selecciona una opcion:
+echo 1. Mostrar la informacion del Sistema
+echo 2. Mostrar la hora actual
+echo 3. Salir
+echo.
+echo Hasta aqui llega la version Beta del Hola mundo!
+pause > nul
+exit
